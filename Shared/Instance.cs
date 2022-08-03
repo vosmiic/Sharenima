c856490c9978cea63f890fd11f19d84503579ffc
vosmiic <44092756+vosmiic@users.noreply.github.com>
2022-08-03 21:26:38 +0100

    added queues to instance

--- Shared/Instance.cs
+++ Shared/Instance.cs
@@ -1,6 +1,13 @@
+using System.Collections.ObjectModel;
+
 namespace Sharenima.Shared; 
 
 public class Instance : Base {
+    private ICollection<Queue> videoQueues;
     public string Name { get; set; }
     public Guid CreateById { get; set; }
+    public ICollection<Queue> VideoQueue {             
+        get => videoQueues ?? (videoQueues = new Collection<Queue>());
+        protected set => videoQueues = value;
+    }
 }
