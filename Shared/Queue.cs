a604f1972971067ad43cacd2d79142d81f1c5506
vosmiic <44092756+vosmiic@users.noreply.github.com>
2022-08-06 13:21:59 +0100

    adding thumbnail to queue class

--- Shared/Queue.cs
+++ Shared/Queue.cs
@@ -8,4 +8,5 @@ public class Queue : Base {
     public Guid AddedById { get; set; }
     public string Url { get; set; }
     public string Name { get; set; }
+    public string? Thumbnail { get; set; }
 }
