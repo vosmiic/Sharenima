a7ca1d1fb2fab7c6e76c3940eac70f5f7f896044
vosmiic <44092756+vosmiic@users.noreply.github.com>
2022-09-11 13:18:30 +0100

    added base handler

--- Server/Handlers/AdministratorHandler.cs
+++ Server/Handlers/AdministratorHandler.cs
@@ -20,32 +20,8 @@ public class AdministratorHandler : AuthorizationHandler<AdministratorRequiremen
         _context = context;
     }
 
-    protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, AdministratorRequirement requirement) {
-        string currentUserId = context.User.FindFirstValue(ClaimTypes.NameIdentifier);
-        ApplicationUser? user = _context.Users.Where(user => user.Id == currentUserId).Include(au => au.Roles).FirstOrDefault();
-        Guid? instanceId = null;
-        
-        if (context.Resource is HttpContext httpContext) {
-            var queryCollection = httpContext.Request.Query;
-            if (queryCollection.ContainsKey("instanceId")) {
-                instanceId = Guid.Parse(queryCollection["instanceId"]);
-            }
-        }
-
-        if (instanceId == null || user == null) {
-            context.Fail();
-            return Task.CompletedTask;
-        }
-        
-        if (user.Roles.Count > 0 && 
-            user.Roles.Any(item => item.InstanceId == instanceId.Value && item.Permission == Permission.Administrator)) {
-            context.Succeed(requirement);
-        } else {
-            context.Fail();
-        }
-
-        return Task.CompletedTask;
-    }
+    protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, AdministratorRequirement requirement) =>
+        BaseHandler.BaseAuthorizationHandler(context, requirement, _context);
 }
 
 internal class AdministratorAuthorizeAttribute : AuthorizeAttribute {
---
+++ Server/Handlers/BaseHandler.cs
@@ -0,0 +1,37 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
+using Sharenima.Server.Data;
+using Sharenima.Server.Models;
+using Sharenima.Shared;
+
+namespace Sharenima.Server.Handlers; 
+
+public class BaseHandler {
+    public static Task BaseAuthorizationHandler(AuthorizationHandlerContext context, AdministratorRequirement requirement, ApplicationDbContext applicationDbContext, Permissions.Permission? additionalPermission = null) {
+        string currentUserId = context.User.FindFirstValue(ClaimTypes.NameIdentifier);
+        ApplicationUser? user = applicationDbContext.Users.Where(user => user.Id == currentUserId).Include(au => au.Roles).FirstOrDefault();
+        Guid? instanceId = null;
+        
+        if (context.Resource is HttpContext httpContext) {
+            var queryCollection = httpContext.Request.Query;
+            if (queryCollection.ContainsKey("instanceId")) {
+                instanceId = Guid.Parse(queryCollection["instanceId"]);
+            }
+        }
+
+        if (instanceId == null || user == null) {
+            context.Fail();
+            return Task.CompletedTask;
+        }
+        
+        if (user.Roles.Count > 0 && 
+            user.Roles.Any(item => item.InstanceId == instanceId.Value && (item.Permission == Permissions.Permission.Administrator || (additionalPermission != null && item.Permission == additionalPermission)))) {
+            context.Succeed(requirement);
+        } else {
+            context.Fail();
+        }
+
+        return Task.CompletedTask;
+    }
+}
