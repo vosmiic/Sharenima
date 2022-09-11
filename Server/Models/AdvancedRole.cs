d3ab9a59f9b11882e4a4ef0e3dd246c5c2eb1f9b
vosmiic <44092756+vosmiic@users.noreply.github.com>
2022-09-11 13:20:06 +0100

    exported permission list to its own class

--- Server/Models/AdvancedRole.cs
+++ Server/Models/AdvancedRole.cs
@@ -6,9 +6,5 @@ public class AdvancedRole : Base {
     public virtual ApplicationUser User { get; set; }
     public string UserId { get; set; }
     public Guid InstanceId { get; set; }
-    public Permission Permission { get; set; }
-}
-
-public enum Permission {
-    Administrator
+    public Permissions.Permission Permission { get; set; }
 }
---
+++ Shared/Permissions.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Sharenima.Shared; 
+
+public class Permissions {
+    public enum Permission {
+        [Display(Name = "Administrator")]
+        Administrator,
+        [Display(Name = "Change video progress")]
+        ChangeProgress,
+        [Display(Name = "Skip video")]
+        SkipVideo,
+        [Display(Name = "Upload videos")]
+        UploadVideo
+    }
+}
