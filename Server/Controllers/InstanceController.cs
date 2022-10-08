a8c92933fed274f3e8f68e976fd4610030da33cb
vosmiic <44092756+vosmiic@users.noreply.github.com>
2022-10-08 13:41:50 +0100

    added list instance user rest call

--- Server/Controllers/InstanceController.cs
+++ Server/Controllers/InstanceController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Sharenima.Server.Data;
 using Sharenima.Server.Models;
+using Sharenima.Server.SignalR;
 using Sharenima.Shared;
 
 namespace Sharenima.Server.Controllers; 
@@ -14,11 +15,13 @@ namespace Sharenima.Server.Controllers;
 public class InstanceController : ControllerBase {
     private readonly IDbContextFactory<GeneralDbContext> _contextFactory;
     private readonly UserManager<ApplicationUser> _userManager;
+    private readonly ConnectionMapping _connectionMapping;
 
     public InstanceController(IDbContextFactory<GeneralDbContext> contextFactory,
-        UserManager<ApplicationUser> userManager) {
+        UserManager<ApplicationUser> userManager, ConnectionMapping connectionMapping) {
         _contextFactory = contextFactory;
         _userManager = userManager;
+        _connectionMapping = connectionMapping;
     }
 
     /// <summary>
@@ -50,5 +53,13 @@ public class InstanceController : ControllerBase {
         Instance? instance = await context.Instances.FirstOrDefaultAsync(instance => instance.Name == instanceName);
 
         return instance != null ? Ok(instance) : NotFound();
+    }
+
+    [HttpGet]
+    [Route("users")]
+    public ActionResult GetInstanceUsers(Guid instanceId) {
+        var instanceConnections = _connectionMapping.GetConnections().FirstOrDefault(item => item.Key == instanceId);
+        if (instanceConnections.Equals(new KeyValuePair<Guid, List<ConnectionMapping.InstanceConnection>>())) return new NoContentResult();
+        return Ok(instanceConnections.Value.Select(item => item.UserName));
     }
 }
