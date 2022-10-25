a87e045c268007954d52e6fc8ae23f047ca7e426
vosmiic <44092756+vosmiic@users.noreply.github.com>
2022-10-25 21:34:08 +0100

    changed when player and queue state is refreshed

--- Client/ComponentCode/Instance/Queue.cs
+++ Client/ComponentCode/Instance/Queue.cs
@@ -6,30 +6,20 @@ using Microsoft.AspNetCore.SignalR.Client;
 using Sharenima.Shared.Helpers;
 using File = Sharenima.Shared.File;
 
-namespace Sharenima.Client.ComponentCode; 
+namespace Sharenima.Client.ComponentCode;
 
 public partial class Queue : ComponentBase {
-    [CascadingParameter]
-    private Task<AuthenticationState> authenticationStateTask { get; set; }
-    [Inject]
-    private IHttpClientFactory HttpClientFactory { get; set; }
-    [Inject]
-    protected IMatToaster _toaster { get; set; }
-    [Inject]
-    private NavigationManager _navigationManager { get; set; }
-    [Inject]
-    private QueuePlayerService QueuePlayerService { get; set; }
-    [Parameter]
-    public Guid InstanceId { get; set; }
-    [Parameter]
-    public ICollection<Sharenima.Shared.Queue>? CurrentQueue { get; set; }
-    [Parameter]
-    public EventCallback<ICollection<Sharenima.Shared.Queue>?> CurrentQueueChanged { get; set; }
-    [Parameter]
-    public Player? PlayerSibling { get; set; }
+    [CascadingParameter] private Task<AuthenticationState> authenticationStateTask { get; set; }
+    [Inject] private IHttpClientFactory HttpClientFactory { get; set; }
+    [Inject] protected IMatToaster _toaster { get; set; }
+    [Inject] private NavigationManager _navigationManager { get; set; }
+    [Inject] protected QueuePlayerService QueuePlayerService { get; set; }
+    [Inject] protected RefreshService RefreshService { get; set; }
+    [Parameter] public Guid InstanceId { get; set; }
+    [Parameter] public EventCallback<ICollection<Sharenima.Shared.Queue>?> CurrentQueueChanged { get; set; }
+    [Parameter] public Player? PlayerSibling { get; set; }
     public string VideoUrl { get; set; }
-    [Parameter]
-    public HubConnection? HubConnection { get; set; }
+    [Parameter] public HubConnection? HubConnection { get; set; }
     private HttpClient? _authHttpClient { get; set; }
     private HttpClient _anonymousHttpClient { get; set; }
 
@@ -39,7 +29,7 @@ public partial class Queue : ComponentBase {
             _authHttpClient = HttpClientFactory.CreateClient("auth");
         _anonymousHttpClient = HttpClientFactory.CreateClient("anonymous");
         await GetQueue();
-        
+
         await Hub();
     }
 
@@ -71,7 +61,7 @@ public partial class Queue : ComponentBase {
                 MediaType = matFileUploadEntry.Type
             };
             var uploadVideoResponse = await _authHttpClient.PostAsync($"queue/fileUpload?instanceId={InstanceId}&connectionId={HubConnection?.ConnectionId}", JsonConverters.ConvertObjectToHttpContent(file));
-            
+
 
             if (!uploadVideoResponse.IsSuccessStatusCode) {
                 _toaster.Add($"Could not upload video; {uploadVideoResponse.ReasonPhrase}", MatToastType.Danger, "Upload Error");
@@ -95,13 +85,15 @@ public partial class Queue : ComponentBase {
         if (!httpResponse.IsSuccessStatusCode) {
             _toaster.Add("Could not load video queue", MatToastType.Danger, "Error");
         }
-        
+
         ICollection<Sharenima.Shared.Queue>? queueCollection = await httpResponse.Content.ReadFromJsonAsync<ICollection<Sharenima.Shared.Queue>>();
         if (queueCollection != null) {
             await CurrentQueueChanged.InvokeAsync(queueCollection);
-            /*CurrentQueue = queueCollection;
-            Console.WriteLine(CurrentQueue?.FirstOrDefault()?.Url);*/
-            QueuePlayerService.CallRequestRefresh();
+            QueuePlayerService.SetQueue(queueCollection.ToList());
+            if (queueCollection.Count > 0) {
+                RefreshService.CallPlayerRefreshRequested();
+                RefreshService.CallInstanceIndexRefresh();
+            }
         }
     }
 
@@ -109,38 +101,28 @@ public partial class Queue : ComponentBase {
         await HubConnection.SendAsync("JoinGroup", InstanceId.ToString());
 
         HubConnection.On<Sharenima.Shared.Queue>("AnnounceVideo", async (queue) => {
-            if (CurrentQueue != null) {
-                CurrentQueue.Add(queue);
-            } else {
-                CurrentQueue = new List<Sharenima.Shared.Queue> { queue };
-            }
-            await CurrentQueueChanged.InvokeAsync(CurrentQueue);
+            QueuePlayerService.AddToQueue(queue);
             StateHasChanged();
-            if (CurrentQueue.Count == 1)
-                QueuePlayerService.CallRequestRefresh();
+            if (QueuePlayerService.CurrentQueue.Count == 1)
+                RefreshService.CallPlayerRefreshRequested();
         });
 
         HubConnection.On<Guid>("RemoveVideo", async (queueId) => {
-            Sharenima.Shared.Queue? queue = CurrentQueue?.FirstOrDefault(queue => queue.Id == queueId);
+            Sharenima.Shared.Queue? queue = QueuePlayerService.CurrentQueue?.FirstOrDefault(queue => queue.Id == queueId);
             if (queue != null) {
-                CurrentQueue.Remove(queue);
-                await CurrentQueueChanged.InvokeAsync(CurrentQueue);
+                QueuePlayerService.RemoveFromQueue(queue);
                 StateHasChanged();
             }
         });
 
-        HubConnection.On<int>("ReceiveStateChange", async (state) => {
+        HubConnection.On<int>("ReceiveStateChange", (state) => {
             if (state == 0) {
-                CurrentQueue.Remove(CurrentQueue.First());
-                var nextVideo = CurrentQueue.FirstOrDefault();
-                if (nextVideo != null) {
-                    QueuePlayerService.SetCurrentQueueVideo(nextVideo);
-                }
-                await CurrentQueueChanged.InvokeAsync(CurrentQueue);
+                // remove current video since it has been completed
+                Console.WriteLine("Video has ended");
+                QueuePlayerService.RemoveFromQueue(QueuePlayerService.CurrentQueue.First());
+                RefreshService.CallPlayerRefreshRequested();
                 StateHasChanged();
             }
         });
-        
-        
     }
 }
--- Client/Program.cs
+++ Client/Program.cs
@@ -16,6 +16,7 @@ builder.Services.AddHttpClient("anonymous", client => client.BaseAddress = new U
 // Supply HttpClient instances that include access tokens when making requests to the server project
 builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("Sharenima.ServerAPI"));
 builder.Services.AddScoped<QueuePlayerService>();
+builder.Services.AddScoped<RefreshService>();
 
 builder.Services.AddApiAuthorization();
 builder.Services.AddMatBlazor();
