1e9c11f611cca41ba5a66a638f12d8c7bea440e2
vosmiic <44092756+vosmiic@users.noreply.github.com>
2023-01-02 21:31:43 +0000

    providing users permissions when accessing instance

--- Server/Controllers/InstanceController.cs
+++ Server/Controllers/InstanceController.cs
@@ -3,28 +3,31 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 using Sharenima.Server.Data;
 using Sharenima.Server.Models;
 using Sharenima.Server.SignalR;
 using Sharenima.Shared;
 
-namespace Sharenima.Server.Controllers; 
+namespace Sharenima.Server.Controllers;
 
 [ApiController]
 [Route("[controller]")]
 public class InstanceController : ControllerBase {
     private readonly IDbContextFactory<GeneralDbContext> _contextFactory;
+    private readonly IDbContextFactory<ApplicationDbContext> _applicationDbContextFactory;
     private readonly UserManager<ApplicationUser> _userManager;
     private readonly ConnectionMapping _connectionMapping;
     private readonly ILogger<InstanceController> _logger;
 
     public InstanceController(IDbContextFactory<GeneralDbContext> contextFactory,
         UserManager<ApplicationUser> userManager, ConnectionMapping connectionMapping,
-        ILogger<InstanceController> logger) {
+        ILogger<InstanceController> logger, IDbContextFactory<ApplicationDbContext> applicationDbContextFactory) {
         _contextFactory = contextFactory;
         _userManager = userManager;
         _connectionMapping = connectionMapping;
         _logger = logger;
+        _applicationDbContextFactory = applicationDbContextFactory;
     }
 
     /// <summary>
@@ -46,15 +49,32 @@ public class InstanceController : ControllerBase {
         context.Instances.Add(instance);
 
         await context.SaveChangesAsync();
-        
+
         _logger.LogInformation($"Instance {instance.Id} created by user {userId}");
         return Ok(instance);
     }
 
     [HttpGet]
-    public async Task<ActionResult> GetInstanceFromName(string instanceName) {
-        await using var context = await _contextFactory.CreateDbContextAsync();
-        Instance? instance = await context.Instances.FirstOrDefaultAsync(instance => instance.Name == instanceName);
+    public async Task<ActionResult> GetInstanceFromName(string instanceName, bool includePermissions) {
+        await using var generalDbContext = await _contextFactory.CreateDbContextAsync();
+        Instance? instance = await generalDbContext.Instances.Where(instance => instance.Name == instanceName).Include(instance => instance.Permissions).FirstOrDefaultAsync();
+        if (includePermissions && instance != null) {
+            InstanceWithUserPermissions instanceWithUserPermissions = JsonConvert.DeserializeObject<InstanceWithUserPermissions>(JsonConvert.SerializeObject(instance));
+            await using var context = await _applicationDbContextFactory.CreateDbContextAsync();
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            if (userId == null) {
+                instanceWithUserPermissions.UserPermissions = instance.Permissions.Where(ip => ip.AnonymousUser).Select(ip => ip.Permissions).ToList();
+            } else {
+                instanceWithUserPermissions.UserPermissions = instance.Permissions.Where(ip => ip.AnonymousUser == false).Select(ip => ip.Permissions).ToList();
+                var advancedRoles = context.AdvancedRoles.Where(item => item.UserId == userId).Select(item => item.Permission).ToList();
+                foreach (Permissions.Permission permission in advancedRoles) {
+                    if (!instanceWithUserPermissions.UserPermissions.Contains(permission))
+                        instanceWithUserPermissions.UserPermissions.Add(permission);
+                }
+            }
+
+            return Ok(instanceWithUserPermissions);
+        }
 
         return instance != null ? Ok(instance) : NotFound();
     }
