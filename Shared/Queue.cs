114a5c88ef203b5ed2f59fad75918812eb6b64ca
vosmiic <44092756+vosmiic@users.noreply.github.com>
2023-01-26 21:06:48 +0000

    adding order to queue

--- Shared/Queue.cs
+++ Shared/Queue.cs
@@ -1,16 +1,16 @@
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace Sharenima.Shared; 
+namespace Sharenima.Shared;
 
 public class Queue : Base {
-    [ForeignKey("Instance")]
-    public Guid InstanceId { get; set; }
+    [ForeignKey("Instance")] public Guid InstanceId { get; set; }
     public Guid AddedById { get; set; }
     public VideoType VideoType { get; set; }
     public string Url { get; set; }
     public string Name { get; set; }
     public string? Thumbnail { get; set; }
     public string? MediaType { get; set; }
+    public int Order { get; set; }
 }
 
 public enum VideoType {
