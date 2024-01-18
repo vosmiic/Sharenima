236f47941c81b7044eda38ced6c0f632625b0ade
vosmiic <44092756+vosmiic@users.noreply.github.com>
2024-01-17 19:29:27 -0500

    added CanBecomeLeader

--- Server/Services/ConnectionMapping.cs
+++ Server/Services/ConnectionMapping.cs
@@ -31,7 +31,8 @@ public class ConnectionMapping {
                         UserId = userId,
                         UserName = userName,
                         IsLeader = true,
-                        LeaderRank = leaderRank
+                        LeaderRank = leaderRank,
+                        CanBecomeLeader = true
                     }
                 });
             }
@@ -45,7 +46,8 @@ public class ConnectionMapping {
                 UserId = userId,
                 UserName = userName,
                 IsLeader = instanceConnections.Value.Value.Count == 0 || instanceConnections.Value.Value.Any(connection => connection.IsLeader && connection.LeaderRank < leaderRank),
-                LeaderRank = leaderRank
+                LeaderRank = leaderRank,
+                CanBecomeLeader = true
             };
             InstanceConnection? oldLeader = null;
             if (newConnection.IsLeader) {
@@ -96,11 +98,62 @@ public class ConnectionMapping {
         return null;
     }
 
+    /// <summary>
+    /// Revoke leadership for user.
+    /// </summary>
+    /// <param name="instanceId">ID of the instance the connection belongs to.</param>
+    /// <param name="connectionId">ID of the connection to revoke leadership of.</param>
+    /// <param name="disableLeader">True to remove connection from potential leadership pool.</param>
+    public void RevokeLeadership(Guid instanceId, string connectionId, bool disableLeader) {
+        InstanceConnection? connection = GetConnectionById(instanceId, connectionId);
+        if (connection == null) return;
+        connection.IsLeader = false;
+        Console.WriteLine($"{connectionId} is no longer leader");
+        if (disableLeader) {
+            connection.CanBecomeLeader = false;
+        }
+
+        ReAllocateLeader(instanceId);
+    }
+
+    /// <summary>
+    /// Allow user to become a leader.
+    /// </summary>
+    /// <param name="instanceId">ID of the instance the connection belongs to.</param>
+    /// <param name="connectionId">ID of the connection to add to the pool of potential leaders.</param>
+    /// <param name="attemptToPromote">True to attempt to promote the connection to leader.</param>
+    public void EnableLeader(Guid instanceId, string connectionId, bool attemptToPromote) {
+        InstanceConnection? connection = GetConnectionById(instanceId, connectionId);
+        if (connection == null) return;
+        connection.CanBecomeLeader = true;
+        if (attemptToPromote) {
+            ReAllocateLeader(instanceId);
+        }
+    }
+
+    private void ReAllocateLeader(Guid instanceId) {
+        var instanceConnections = _instanceConnections.FirstOrDefault(item => item.Key == instanceId);
+        if (!instanceConnections.Equals(new KeyValuePair<Guid, List<InstanceConnection>>())) {
+            var connectionsThatCanBecomeLeader = instanceConnections.Value.Where(item => item.CanBecomeLeader).ToList();
+            if (connectionsThatCanBecomeLeader.Count > 0) {
+                var newLeader = connectionsThatCanBecomeLeader.MaxBy(item => item.LeaderRank);
+                if (newLeader != null && !newLeader.IsLeader) {
+                    var oldLeader = instanceConnections.Value.FirstOrDefault(item => item.IsLeader);
+                    if (oldLeader != null) {
+                        oldLeader.IsLeader = false;
+                    }
+                    newLeader.IsLeader = true;
+                }
+            }
+        }
+    }
+
     public class InstanceConnection {
         public Guid? UserId { get; set; }
         public string UserName { get; set; }
         public string ConnectionId { get; set; }
-        public bool IsLeader {get;set;}
+        public bool IsLeader { get; set; }
         public int LeaderRank { get; set; }
+        public bool CanBecomeLeader { get; set; }
     }
 }
