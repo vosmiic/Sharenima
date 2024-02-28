bac283a84dda5ce3045d2fce4ac2453ee847aa9c
vosmiic <44092756+vosmiic@users.noreply.github.com>
2024-02-27 21:35:43 -0500

    adding video time redis key string

--- Server/Helpers/RedisHelper.cs
+++ Server/Helpers/RedisHelper.cs
@@ -2,4 +2,5 @@ namespace Sharenima.Server.Helpers;
 
 public class RedisHelper {
     public static string InstanceStateChangeKey(Guid instanceId) => $"instance:{instanceId}:statechangelock";
+    public static string InstanceVideoTimeKey(Guid instanceId) => $"instance:{instanceId}:videotime";
 }
