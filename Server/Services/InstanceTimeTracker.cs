62ad27fbf0657049da5edb37f49c67effd1031a2
vosmiic <44092756+vosmiic@users.noreply.github.com>
2024-02-27 21:30:30 -0500

    storing instance times in redis

--- Server/Services/InstanceTimeTracker.cs
+++ Server/Services/InstanceTimeTracker.cs
@@ -1,42 +1,36 @@
+using Sharenima.Server.Helpers;
+using StackExchange.Redis;
+
 namespace Sharenima.Server.Services;
 
 public class InstanceTimeTracker {
     private readonly ILogger<InstanceTimeTracker> _logger;
+    private readonly IConnectionMultiplexer _connectionMultiplexer;
 
-    public InstanceTimeTracker(ILogger<InstanceTimeTracker> logger) {
-        _logger = logger;
-    }
-
-    private List<(Guid instanceId, TimeSpan instanceTime)> _instanceTimes { get; set; } =
-        new();
-
-    /// <summary>
-    /// Add instance to list of instance times.
-    /// </summary>
-    /// <param name="instanceId">Instance ID to add to the list.</param>
-    /// <param name="instanceTime">Time of the instance.</param>
-    public void Add(Guid instanceId, TimeSpan instanceTime) {
-        if (_instanceTimes.FirstOrDefault(instanceTime => instanceTime.instanceId == instanceId).Equals(default))
-            _instanceTimes.Add((instanceId, instanceTime));
+    public InstanceTimeTracker(ILoggerFactory loggerFactory,
+        IConnectionMultiplexer connectionMultiplexer) {
+        _logger = loggerFactory.CreateLogger<InstanceTimeTracker>();
+        _connectionMultiplexer = connectionMultiplexer;
     }
 
     /// <summary>
-    /// Update instance time.
+    /// Add or update instance time.
     /// </summary>
-    /// <param name="instanceId">Instance ID to update.</param>
-    /// <param name="newInstanceTime">New instance time to set to.</param>
-    /// <param name="createNewIfNotExist">True to add the instance to the list if it does not already exist.</param>
-    public void Update(Guid instanceId, TimeSpan newInstanceTime, bool createNewIfNotExist = false) {
-        (Guid instanceId, TimeSpan instanceTime) instanceTime = _instanceTimes.FirstOrDefault(instanceTime => instanceTime.instanceId == instanceId);
-        if (!instanceTime.Equals(default)) {
-            lock (_instanceTimes) {
-                _instanceTimes.Remove(instanceTime);
-                Add(instanceId, newInstanceTime);
-            }
-        } else if (createNewIfNotExist) {
-            Add(instanceId, newInstanceTime);
-        }
-        _logger.LogTrace($"Update instance {instanceId} time to {newInstanceTime}");
+    /// <param name="instanceId">Instance ID that the video time belongs to.</param>
+    /// <param name="videoTime">Time of the video to set.</param>
+    public void Upsert(Guid instanceId, TimeSpan videoTime) {
+        IDatabase database = _connectionMultiplexer.GetDatabase();
+        database.SetAdd(RedisHelper.InstanceVideoTimeKey(instanceId), videoTime.TotalMilliseconds);
+        _logger.LogInformation($"Set/updated instance {instanceId} video time to {videoTime}.");
+    }
+
+    /// <summary>
+    /// Delete instance video time.
+    /// </summary>
+    /// <param name="instanceId">ID of the instance to delete the video time of.</param>
+    public void Remove(Guid instanceId) {
+        IDatabase database = _connectionMultiplexer.GetDatabase();
+        database.KeyDelete(RedisHelper.InstanceVideoTimeKey(instanceId));
     }
 
     /// <summary>
@@ -45,7 +39,12 @@ public class InstanceTimeTracker {
     /// <param name="instanceId">Instance ID to get the time of.</param>
     /// <returns>Instance time or null if value does not exist.</returns>
     public TimeSpan? GetInstanceTime(Guid instanceId) {
-        (Guid instanceId, TimeSpan instanceTime) instanceTime = _instanceTimes.FirstOrDefault(instanceTime => instanceTime.instanceId == instanceId);
-        return instanceTime.Equals(default) ? null : instanceTime.instanceTime;
+        IDatabase database = _connectionMultiplexer.GetDatabase();
+        RedisValue instanceTime = database.StringGet(RedisHelper.InstanceVideoTimeKey(instanceId));
+        return instanceTime.HasValue ? 
+            TimeSpan.TryParse(instanceTime, out TimeSpan parsedTime) ?
+                parsedTime :
+                null :
+            null;
     }
 }
