69282fdd640274a7b52b82fe76a02ceed36ec317
vosmiic <44092756+vosmiic@users.noreply.github.com>
2024-03-21 19:32:01 -0400

    removing ffmpeg libraries

--- Server/Helpers/FfmpegHelper.cs
+++ Server/Helpers/FfmpegHelper.cs
@@ -1,101 +1,45 @@
-using Xabe.FFmpeg;
+using System.Diagnostics;
 
 namespace Sharenima.Server.Helpers;
 
 public class FfmpegHelper {
-    public static async Task<string?> FfmpegCommand(bool ffmpegCommand, string command) {
-        if (ffmpegCommand) {
-            IConversionResult? ffmpegResult = await FFmpeg.Conversions.New().AddParameter(command).Start();
-            return ffmpegResult.Arguments;
-        }
+    public static async Task<(string result, bool success, string errorReason)> RunFfprobeCommand(string argument) =>
+        await FfprobeCommand(argument);
+    public static async Task<(string result, bool success, string errorReason)> RunFfprobeCommand(string argument, string input, FfmpegFormat inputFormat) =>
+        await FfprobeCommand(argument, input: input, inputFormat: inputFormat);
 
-        string? ffprobeResult = await Probe.New().Start(command);
-        return ffprobeResult;
+    private static async Task<(string result, bool success, string errorReason)> FfprobeCommand(string argument, string? input = null, FfmpegFormat? inputFormat = null) {
+        string errorResult = String.Empty;
+
+        Process proc = await RunConsoleCommand($"{(input != null ? $"{input} | " : String.Empty)}ffprobe {argument} -f ");
+        string result = await proc.StandardOutput.ReadToEndAsync();
+        errorResult += await proc.StandardError.ReadToEndAsync();
+
+        await proc.WaitForExitAsync();
+
+        return (result, errorResult != String.Empty, errorResult);
     }
 
-    public static async Task<bool> ConvertVideo(ILogger logger, string videoFilePath, string convertedVideoOutputPath, VideoCodec videoCodec) => await ConvertMedia(logger, videoFilePath, convertedVideoOutputPath, videoCodec: videoCodec);
-    public static async Task<bool> ConvertAudio(ILogger logger, string audioFilePath, string convertedAudioOutputPath, AudioCodec audioCodec) => await ConvertMedia(logger, audioFilePath, convertedAudioOutputPath, audioCodec: audioCodec);
+    public static async Task<(Stream stream, bool success, string errorReason)> RunFfmpegCommand(string argument, FfmpegFormat ffmpegFormat, string? input = null) {
+        string errorResult = String.Empty;
 
-    private static async Task<bool> ConvertMedia(ILogger logger, string mediaFilePath, string convertedMediaOutputPath, VideoCodec? videoCodec = null, AudioCodec? audioCodec = null) {
-        IMediaInfo mediaInfo = await FFmpeg.GetMediaInfo(mediaFilePath);
+        Process proc = await RunConsoleCommand($"{(input != null ? $"\"{input}\" | " : String.Empty)}ffmpeg {argument} -f {ffmpegFormat.ToString().ToLower()} pipe:");
+        var memoryStream = proc.StandardOutput.BaseStream;
+        errorResult += await proc.StandardError.ReadToEndAsync();
 
-        IStream? stream = null;
-        if (videoCodec != null) {
-            stream = mediaInfo.VideoStreams.FirstOrDefault()?.SetCodec(videoCodec.Value);
-        }
+        await proc.WaitForExitAsync();
 
-        if (audioCodec != null) {
-            stream = mediaInfo.AudioStreams.FirstOrDefault()?.SetCodec(audioCodec.Value);
-        }
-
-        if (stream == null) {
-            logger.LogInformation("Cannot convert video; no media streams detected");
-            return false;
-        }
-
-        IConversion conversion = FFmpeg.Conversions.New()
-            .AddStream(stream)
-            .SetOutput(convertedMediaOutputPath);
-        if (videoCodec != null) {
-            IStream? audioStream = mediaInfo.AudioStreams.FirstOrDefault();
-            if (audioStream != null)
-                conversion.AddStream(audioStream);
-        }
-
-        try {
-            await conversion.Start();
-        } catch (Exception e) {
-            // todo log e
-            logger.LogError($"Error converting media; {e.Message}");
-            return false;
-        }
-
-        return true;
+        return (memoryStream, errorResult != String.Empty, errorResult);
     }
 
-    public static async Task<(string? outputPath, string? error)> ExtractStreamToFile(ILogger logger, ISubtitleStream subtitleStream, string outputPath) {
-        // checking if file exists; appending counter to name if so
-        if (File.Exists(outputPath)) {
-            int counter = 1;
-            while (true) {
-                string newName = outputPath.Replace(".srt", $" ({counter}).srt");
-                if (File.Exists(newName)) {
-                    counter++;
-                } else {
-                    outputPath = newName;
-                    break;
-                }
-            }
-        }
-        
-        IConversion conversion = FFmpeg.Conversions.New()
-            .AddStream(subtitleStream)
-            .SetOutput(outputPath);
-
-        try {
-            await conversion.Start();
-        } catch (Exception e) {
-            logger.LogError($"Error converting media; {e.Message}");
-            return (null, e.Message);
-        }
-        
-        return (outputPath, null);
-    }
-
-    public static async Task<IMediaInfo> GetFileMetadata(string filePath) {
-        return await FFmpeg.GetMediaInfo(filePath);
-    }
-
-
-    public static async Task<bool> IsFfmpegInstalled(string argument) {
-        string errorResult = "";
-        using System.Diagnostics.Process proc = new System.Diagnostics.Process();
+    private static async Task<Process> RunConsoleCommand(string argument) {
+        Process proc = new Process();
         if (OperatingSystem.IsWindows()) {
             proc.StartInfo.FileName = "cmd.exe";
             proc.StartInfo.Arguments = argument;
         } else {
             proc.StartInfo.FileName = "/bin/bash";
-            proc.StartInfo.Arguments = $"-c \" {argument} \"";
+            proc.StartInfo.Arguments = $"-c \" {argument}\"";
         }
 
         proc.StartInfo.UseShellExecute = false;
@@ -103,10 +47,10 @@ public class FfmpegHelper {
         proc.StartInfo.RedirectStandardError = true;
         proc.Start();
 
-        errorResult += await proc.StandardError.ReadToEndAsync();
+        return proc;
+    }
 
-        await proc.WaitForExitAsync();
-
-        return string.IsNullOrEmpty(errorResult);
+    public enum FfmpegFormat {
+        Webm
     }
 }
