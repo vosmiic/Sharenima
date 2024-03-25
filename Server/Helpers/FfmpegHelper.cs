a577a974041ff3b38a3d184c909967cd7be60f1b
vosmiic <44092756+vosmiic@users.noreply.github.com>
2024-03-25 19:58:28 -0400

    added ffprobe metadata call

--- Server/Helpers/FfmpegHelper.cs
+++ Server/Helpers/FfmpegHelper.cs
@@ -1,56 +1,19 @@
-using System.Diagnostics;
+using System.Text.Json;
+using Sharenima.Server.Models;
 
 namespace Sharenima.Server.Helpers;
 
 public class FfmpegHelper {
-    public static async Task<(string result, bool success, string errorReason)> RunFfprobeCommand(string argument) =>
-        await FfprobeCommand(argument);
-    public static async Task<(string result, bool success, string errorReason)> RunFfprobeCommand(string argument, string input, FfmpegFormat inputFormat) =>
-        await FfprobeCommand(argument, input: input, inputFormat: inputFormat);
-
-    private static async Task<(string result, bool success, string errorReason)> FfprobeCommand(string argument, string? input = null, FfmpegFormat? inputFormat = null) {
-        string errorResult = String.Empty;
-
-        Process proc = await RunConsoleCommand($"{(input != null ? $"{input} | " : String.Empty)}ffprobe {argument} -f ");
-        string result = await proc.StandardOutput.ReadToEndAsync();
-        errorResult += await proc.StandardError.ReadToEndAsync();
-
-        await proc.WaitForExitAsync();
-
-        return (result, errorResult != String.Empty, errorResult);
+    public static async Task<FfprobeMetadata.Metadata?> GetMetadata(string fileLocation) {
+        (string result, bool success, string errorReason) response = await FfmpegCore.RunFfprobeCommand($"ffprobe -v quiet -print_format json -show_format -show_streams \"{fileLocation}\"");
+        if (!response.success) return null;
+        FfprobeMetadata.Metadata? metadata = JsonSerializer.Deserialize<FfprobeMetadata.Metadata>(response.result);
+        return metadata;
     }
 
-    public static async Task<(Stream stream, bool success, string errorReason)> RunFfmpegCommand(string argument, FfmpegFormat ffmpegFormat, string? input = null) {
-        string errorResult = String.Empty;
-
-        Process proc = await RunConsoleCommand($"{(input != null ? $"\"{input}\" | " : String.Empty)}ffmpeg {argument} -f {ffmpegFormat.ToString().ToLower()} pipe:");
-        var memoryStream = proc.StandardOutput.BaseStream;
-        errorResult += await proc.StandardError.ReadToEndAsync();
-
-        await proc.WaitForExitAsync();
-
-        return (memoryStream, errorResult != String.Empty, errorResult);
+    public static void GetMetadata(MemoryStream memoryStream) {
+        
     }
-
-    private static async Task<Process> RunConsoleCommand(string argument) {
-        Process proc = new Process();
-        if (OperatingSystem.IsWindows()) {
-            proc.StartInfo.FileName = "cmd.exe";
-            proc.StartInfo.Arguments = argument;
-        } else {
-            proc.StartInfo.FileName = "/bin/bash";
-            proc.StartInfo.Arguments = $"-c \" {argument}\"";
-        }
-
-        proc.StartInfo.UseShellExecute = false;
-        proc.StartInfo.RedirectStandardOutput = true;
-        proc.StartInfo.RedirectStandardError = true;
-        proc.Start();
-
-        return proc;
-    }
-
-    public enum FfmpegFormat {
-        Webm
-    }
+    
+    
 }
