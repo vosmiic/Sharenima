1e97a93a876f4e19b86697edb1e4fe5dac054e29
vosmiic <44092756+vosmiic@users.noreply.github.com>
2024-03-29 13:33:36 -0400

    removing unnecessary binary call

--- Server/Helpers/FfmpegHelper.cs
+++ Server/Helpers/FfmpegHelper.cs
@@ -5,7 +5,7 @@ namespace Sharenima.Server.Helpers;
 
 public class FfmpegHelper {
     public static async Task<FfprobeMetadata.Metadata?> GetMetadata(string fileLocation) {
-        (string result, bool success, string errorReason) response = await FfmpegCore.RunFfprobeCommand($"ffprobe -v quiet -print_format json -show_format -show_streams \"{fileLocation}\"");
+        (string result, bool success, string errorReason) response = await FfmpegCore.RunFfprobeCommand($"-v quiet -print_format json -show_format -show_streams \"{fileLocation}\"");
         if (!response.success) return null;
         FfprobeMetadata.Metadata? metadata = JsonSerializer.Deserialize<FfprobeMetadata.Metadata>(response.result);
         return metadata;
