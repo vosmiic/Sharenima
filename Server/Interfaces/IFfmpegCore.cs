e2f140793c093c622e4b46fd3ebf54e20ac0ab39
vosmiic <44092756+vosmiic@users.noreply.github.com>
2024-09-10 21:38:36 +0100

    creating ffmpegcore interface to unit test

---
+++ Server/Interfaces/IFfmpegCore.cs
@@ -0,0 +1,45 @@
+using System.Diagnostics;
+
+namespace Sharenima.Server.Helpers;
+
+public interface IFfmpegCore {
+    /// <summary>
+    /// Run an FFprobe command.
+    /// </summary>
+    /// <param name="argument">Argument to add to the command.</param>
+    /// <returns>Execution result.</returns>
+    Task<string?> RunFfprobeCommand(string argument);
+    /// <summary>
+    /// Run an FFprobe command.
+    /// </summary>
+    /// <param name="argument">Argument to add to the command.</param>
+    /// <param name="input">Input string.</param>
+    /// <param name="inputFormat">Input format of the input media.</param>
+    /// <returns>Execution result.</returns>
+    Task<string?> RunFfprobeCommand(string argument, string input, FfmpegCore.FfmpegFormat inputFormat);
+    /// <summary>
+    /// Run an FFmpeg command.
+    /// </summary>
+    /// <param name="argument">Argument to add to the command.</param>
+    /// <returns>Generated stream, true if the command ran to completion, error reason if failed; null if successful.</returns>
+    Task<MemoryStream?> RunFfmpegCommand(string argument);
+
+    /// <summary>
+    /// Run an FFmpeg command.
+    /// </summary>
+    /// <param name="argument">Argument to add to the command.</param>
+    /// <param name="ffmpegFormat">Output format.</param>
+    /// <param name="output">Output location.</param>
+    /// <returns>Generated stream.</returns>
+    Task<MemoryStream?> RunFfmpegCommand(string argument, FfmpegCore.FfmpegFormat ffmpegFormat, string? output = null);
+
+    /// <summary>
+    /// Run an FFmpeg command
+    /// </summary>
+    /// <param name="argument">Argument to add to the command.</param>
+    /// <param name="input">Input string.</param>
+    /// <param name="ffmpegFormat">Output format.</param>
+    /// <param name="output">Output location.</param>
+    /// <returns>Generated stream.</returns>
+    Task<MemoryStream?> RunFfmpegCommand(string argument, string input, FfmpegCore.FfmpegFormat ffmpegFormat, string? output = null);
+}
